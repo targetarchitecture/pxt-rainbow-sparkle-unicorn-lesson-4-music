41574d35de0dad39ccf7f3ae065fcb674512aa4d
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-10-09 14:02:01 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    RainbowSparkleUnicorn.Sound.setVolume(20)
+    RainbowSparkleUnicorn.Sound.playTrack(26)
+})
+RainbowSparkleUnicorn.start()
 basic.forever(function () {
 	
 })
